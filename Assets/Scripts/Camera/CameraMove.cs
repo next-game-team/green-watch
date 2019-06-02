2a21aadfb657db4c63461495829adde752e47ddc
PsyGameStud <psygamestud@gmail.com>
2019-06-02 22:47:18 +0300

    Create Script - CameraMove

---
+++ Assets/Scripts/Camera/CameraMove.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+    public float camMoveSpeed;
+    public float camBorderThickness;
+
+
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        Vector3 pos = transform.position;
+
+        if(Input.mousePosition.y >= Screen.height - camBorderThickness)
+        {
+            pos.y += camMoveSpeed * Time.deltaTime;
+        } 
+        if(Input.mousePosition.y <= camBorderThickness) {
+            pos.y -= camMoveSpeed * Time.deltaTime;
+        }
+
+        if(Input.mousePosition.x >= Screen.width - camBorderThickness) {
+            pos.x += camMoveSpeed * Time.deltaTime;
+        }
+
+        if(Input.mousePosition.x <= camBorderThickness) {
+            pos.x -= camMoveSpeed * Time.deltaTime;
+        }
+
+        transform.position = pos;
+    }
+}
