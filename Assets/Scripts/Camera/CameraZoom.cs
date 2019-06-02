1be37b2c9451ae7e6ac6909bac4596c95eb3184b
PsyGameStud <psygamestud@gmail.com>
2019-06-02 22:31:08 +0300

    Create Script
    CameraZoom.cs

---
+++ Assets/Scripts/Camera/CameraZoom.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class CameraZoom : MonoBehaviour
+{
+    public float zoomSpeed = 4f;
+	public float minZoom = 5f;
+	public float maxZoom = 15f;
+
+    private float currentZoom = 10f;
+
+    private Camera cam;
+
+    void Start()
+    {
+        cam = GetComponent<Camera>();
+    }
+
+    void Update()
+    {
+        Zoom();
+    }
+
+    private void Zoom()
+    {
+        currentZoom -= Input.GetAxis("Mouse ScrollWheel") * zoomSpeed;
+		currentZoom = Mathf.Clamp(currentZoom, minZoom, maxZoom);
+
+        cam.orthographicSize = currentZoom;
+    }
+}
