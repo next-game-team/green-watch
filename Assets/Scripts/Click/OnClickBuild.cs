7cd100a24f8c8d4e11197dede0edf3b8a92117a6
PsyGameStud <psygamestud@gmail.com>
2019-06-15 18:40:12 +0300

    Create OnClickBuild Script

---
+++ Assets/Scripts/Click/OnClickBuild.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class OnClickBuild : MonoBehaviour
+{
+    [SerializeField] private Animator _animPanel;
+    [SerializeField] private CameraMove _camMove;
+    private bool isStopMove;
+
+    void Start()
+    {
+        isStopMove = false;
+        if(!isStopMove)
+        {
+            _camMove.enabled = true;
+        }
+    }
+
+    void OnMouseDown()
+    {
+        _animPanel.SetTrigger("Open");
+        isStopMove = true;
+        if(isStopMove)
+        {
+            _camMove.enabled = false;
+        }
+    }
+}
