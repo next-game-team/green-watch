0f1d870fb7f1fdc1d9941d57ecff96b9d32367a9
PsyGameStud <psygamestud@gmail.com>
2019-06-15 23:24:15 +0300

    Create Script ButtonsPanelControl

---
+++ Assets/Scripts/Button/ButtonPanelControl.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class ButtonPanelControl : MonoBehaviour
+{
+    public Animator panelBtns;
+    public bool isOpen;
+
+    void Start()
+    {
+        isOpen = true;
+    }
+
+    public void OpenClosePanel()
+    {
+        isOpen = !isOpen;
+        if(!isOpen)
+        {
+            panelBtns.SetTrigger("Close");
+        } else {
+            panelBtns.SetTrigger("Open");
+        }
+    }
+}
