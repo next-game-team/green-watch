b728e5c05aa21bac6336ca4082da0f5e1dfcf8e6
PsyGameStud <psygamestud@gmail.com>
2019-06-16 22:35:07 +0300

    Create script ButtonMoveCamera
    Editor script

---
+++ Assets/Scripts/Camera/ButtonMoveCamera.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(CameraMove))]
+public class ButtonMoveCamera : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+        CameraMove camMove = (CameraMove)target;
+
+        if(GUILayout.Button("On / Off - mouse move"))
+        {
+            camMove.OnOffMoveMouse();
+        }
+    }
+}
