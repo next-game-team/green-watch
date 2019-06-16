58b79449a9c7c6ebd8bc3aa72308d0b5cf7e9fec
PsyGameStud <psygamestud@gmail.com>
2019-06-16 22:34:42 +0300

    Update CameraMove.cs

--- Assets/Scripts/Camera/CameraMove.cs
+++ Assets/Scripts/Camera/CameraMove.cs
@@ -5,6 +5,8 @@ public class CameraMove : MonoBehaviour
     public float camMoveSpeed;
     public float camBorderThickness;
     public Vector2 camPosLimit;
+    
+    [ReadOnly] public bool isMouseMove;
 
 
     void Update()
@@ -12,25 +14,50 @@ public class CameraMove : MonoBehaviour
         Move();
     }
 
+    public void OnOffMoveMouse()
+    {
+        isMouseMove = !isMouseMove;
+    }
+
     private void Move()
     {
         Vector3 pos = transform.position;
 
-        if(Input.GetKey(KeyCode.W) || Input.mousePosition.y >= Screen.height - camBorderThickness)
+        if(isMouseMove)
         {
-            pos.y += camMoveSpeed * Time.deltaTime;
-        } 
-        if(Input.GetKey(KeyCode.S) || Input.mousePosition.y <= camBorderThickness) {
-            pos.y -= camMoveSpeed * Time.deltaTime;
+            if(Input.GetKey(KeyCode.W) || Input.mousePosition.y >= Screen.height - camBorderThickness)
+            {
+                pos.y += camMoveSpeed * Time.deltaTime;
+            } 
+            if(Input.GetKey(KeyCode.S) || Input.mousePosition.y <= camBorderThickness) {
+                pos.y -= camMoveSpeed * Time.deltaTime;
+            }
+
+            if(Input.GetKey(KeyCode.D) || Input.mousePosition.x >= Screen.width - camBorderThickness) {
+                pos.x += camMoveSpeed * Time.deltaTime;
+            }
+
+            if(Input.GetKey(KeyCode.A) || Input.mousePosition.x <= camBorderThickness) {
+                pos.x -= camMoveSpeed * Time.deltaTime;
+            }
+        } else {
+            if(Input.GetKey(KeyCode.W))
+            {
+                pos.y += camMoveSpeed * Time.deltaTime;
+            } 
+            if(Input.GetKey(KeyCode.S)) {
+                pos.y -= camMoveSpeed * Time.deltaTime;
+            }
+
+            if(Input.GetKey(KeyCode.D)) {
+                pos.x += camMoveSpeed * Time.deltaTime;
+            }
+
+            if(Input.GetKey(KeyCode.A)) {
+                pos.x -= camMoveSpeed * Time.deltaTime;
+            }
         }
 
-        if(Input.GetKey(KeyCode.D) || Input.mousePosition.x >= Screen.width - camBorderThickness) {
-            pos.x += camMoveSpeed * Time.deltaTime;
-        }
-
-        if(Input.GetKey(KeyCode.A) || Input.mousePosition.x <= camBorderThickness) {
-            pos.x -= camMoveSpeed * Time.deltaTime;
-        }
 
         pos.x = Mathf.Clamp(pos.x, -camPosLimit.x, camPosLimit.x);
         pos.y = Mathf.Clamp(pos.y, -camPosLimit.y, camPosLimit.y);
