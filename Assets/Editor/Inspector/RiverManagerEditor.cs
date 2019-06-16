962f721d8a5757712de0d53948f69ee9f418fc14
nikchernyakov <nikchernyakov@yandex.ru>
2019-06-16 12:22:58 +0300

    Add River logic

---
+++ Assets/Editor/Inspector/RiverManagerEditor.cs
@@ -0,0 +1,25 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Editor.Inspector
+{
+    [CustomEditor(typeof(RiverManager))]
+    public class RiverManagerEditor : UnityEditor.Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            DrawDefaultInspector();
+        
+            var script = (RiverManager) target;
+            if(GUILayout.Button("Next Stage"))
+            {
+                script.NextStage();
+            }
+            
+            if(GUILayout.Button("Previous Stage"))
+            {
+                script.PreviousStage();
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/River/RiverManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class RiverManager : NatureManager
+{
+
+    private SpriteRenderer _spriteRenderer;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        InitStage();
+    }
+
+    protected override void OnNextStage()
+    {
+        _spriteRenderer.color = RiverStageManager.Instance.GetStageInfo(GetCurrentStage()).RiverColor;
+    }
+
+    protected override void OnPreviousStage()
+    {
+        _spriteRenderer.color = RiverStageManager.Instance.GetStageInfo(GetCurrentStage()).RiverColor;
+    }
+}
